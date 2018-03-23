5a958d5ac677f2ada738c538f7452bb4341142c2
Natasha S Love <debora@inbox.ru>
2018-03-23 16:45:33 -0500

    working to a space

--- CIS2013_Week09_Homework1.cpp
+++ CIS2013_Week09_Homework1.cpp
@@ -15,11 +15,8 @@ int main () {
 	char input;
 	char keychar;
 	
-	//char a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
-	
 	cout << "Hello! Enter the key file number (1, 2, 3)" << endl;
 	cin >> f_name;
-	//string K = "key.dat";
 	
 	if (f_name=='1') {
 	key.open("key.dat"); 
@@ -54,7 +51,7 @@ int main () {
 			char c = encrypt(msg[i], keychar);
 			encryptedStr[i] = c;
 		}
-		//encryptedStr[i] = 0;
+		encryptedStr[i] = 0;
 		cout << encryptedStr<<endl;
 	}
 	key.close();
@@ -71,7 +68,7 @@ int main () {
 		cin >>  secr_msg;
 		
 	{
-		char decryptedStr[500];
+		//char decryptedStr[500];
 		int i;
 		for (i = 0; i < secr_msg.length(); i++)
 		{
@@ -92,17 +89,6 @@ int main () {
 	 }
 	
 
-	
-	// int encrypt;
-	
-	// for(int i=0; i<=msg.length(); i++) {
-	// for(int y=0; y<500; y++) {
-		// key[y]+msg[i]= encrypt;
-	// }
-	
-// }
-//cout << encrypt;
-	
 	
 	return 0;
 }
@@ -138,235 +124,3 @@ char decrypt(char input, char keychar)
 	if (!R) return ' ';
 	return R + 'a' - 1;
 }
-
-
-//cout << "Enter your message: ";
-	//cout << endl;
-	
-	// for (int i=0; i<msg.length(); i++) {
-
-// if (msg[i] = 'a') {
-				// a=1;
-				
-				// }
-				// else if (msg[i]=='b') {
-				// b=2;
-				
-				// }
-				// else if (msg[i]=='c') {
-				// c=3;
-				
-				// }
-				// else if (msg[i]=='d') {
-				// d=4;
-				
-				// }
-				// else if (msg[i]=='e') {
-				// e=5;
-				
-				// }
-				// else if (msg[i]=='f') {
-				// f=6;
-				
-				// }
-				// else if (msg[i]=='g') {
-				// g=7;
-				
-				// }
-				// else if (msg[i]=='h') {
-				// h=8;
-				
-				// }
-				// else if (msg[i]=='i') {
-				// i=9;
-				
-				// }
-				// else if (msg[i]=='j') {
-				// j=10;
-				
-				// }
-				// else if (msg[i]=='k') {
-				// k=11;
-				
-				// }
-				// else if (msg[i]=='l') {
-				// l=12;
-				
-				// }
-				// else if (msg[i]=='m') {
-				// m=13;
-				
-				// }
-				// else if (msg[i]=='n') {
-				// n=14;
-				
-				// }
-				// else if (msg[i]=='o') {
-				// o=15;
-				
-				// }
-				// else if (msg[i]=='p') {
-				// p=16;
-				
-				// }
-				// else if (msg[i]=='q') {
-				// q=17;
-				
-				// }
-				// else if (msg[i]=='r') {
-				// r=18;
-				
-				// }
-				// else if (msg[i]=='s') {
-				// s=19;
-				
-				// }
-				// else if (msg[i]=='t') {
-				// t=20;
-				
-				// }
-				// else if (msg[i]=='u') {
-				// u=21;
-				
-				// }
-				// else if (msg[i]=='v') {
-				// v=22;
-				
-				// }
-				// else if (msg[i]=='w') {
-				// w=23;
-				
-				// }
-				// else if (msg[i]=='x') {
-				// x=24;
-				
-				// }
-				// else if (msg[i]=='y') {
-				// y=25;
-				
-				// }
-				// else if (msg[i]=='z') {
-				// z=26;
-				
-				// }
-				// // else if (msg [i]==' ') {
-				// // ' '=27;
-				
-				// // }
-				
-		
-// }
-	
-// while (key>>symb){
-			
-		
-			// if (symb=='a') {
-				// a=1;
-				
-				// }
-				// else if (symb=='b') {
-				// b=2;
-				
-				// }
-				// else if (symb=='c') {
-				// c=3;
-				
-				// }
-				// else if (symb=='d') {
-				// d=4;
-				
-				// }
-				// else if (symb=='e') {
-				// e=5;
-				
-				// }
-				// else if (symb=='f') {
-				// f=6;
-				
-				// }
-				// else if (symb=='g') {
-				// g=7;
-				
-				// }
-				// else if (symb=='h') {
-				// h=8;
-				
-				// }
-				// else if (symb=='i') {
-				// i=9;
-				
-				// }
-				// else if (symb=='j') {
-				// j=10;
-				
-				// }
-				// else if (symb=='k') {
-				// k=11;
-				
-				// }
-				// else if (symb=='l') {
-				// l=12;
-				
-				// }
-				// else if (symb=='m') {
-				// m=13;
-				
-				// }
-				// else if (symb=='n') {
-				// n=14;
-				
-				// }
-				// else if (symb=='o') {
-				// o=15;
-				
-				// }
-				// else if (symb=='p') {
-				// p=16;
-				
-				// }
-				// else if (symb=='q') {
-				// q=17;
-				
-				// }
-				// else if (symb=='r') {
-				// r=18;
-				
-				// }
-				// else if (symb=='s') {
-				// s=19;
-				
-				// }
-				// else if (symb=='t') {
-				// t=20;
-				
-				// }
-				// else if (symb=='u') {
-				// u=21;
-				
-				// }
-				// else if (symb=='v') {
-				// v=22;
-				
-				// }
-				// else if (symb=='w') {
-				// w=23;
-				
-				// }
-				// else if (symb=='x') {
-				// x=24;
-				
-				// }
-				// else if (symb=='y') {
-				// y=25;
-				
-				// }
-				// else if (symb=='z') {
-				// z=26;
-				
-				// }
-				// else if (symb==' ') {
-				// ' '=27;
-				
-				// }
-				
-		//}
